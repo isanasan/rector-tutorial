b54cfd1ed15ed63a71a1e917da0854215dcbd923
isanasan <yfcgpsebp@gmail.com>
2022-06-25 14:28:43 +0900

    feat: own rule

--- rules/RectorTutorial/Rector/ClassMethod/AddTestAnnotationRector.php
+++ rules/RectorTutorial/Rector/ClassMethod/AddTestAnnotationRector.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace Rector\RectorTutorial\Rector\ClassMethod;
 
 use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\ClassLike;
+use PHPUnit\Framework\TestCase;
+use PHPStan\Type\ObjectType;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\GenericTagValueNode;
 use Rector\Core\Rector\AbstractRector;
+use Rector\BetterPhpDocParser\ValueObject\PhpDocNode\PHPUnit\PHPUnitTest;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -52,7 +59,42 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
+        if (!$this->shouldRefactor($node)) {
+            return null;
+        }
+
+        // メソッドの PHP Doc を取得
+        $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($node);
+
+        $phpDocInfo->addPhpDocTagNode(
+            new PhpDocTagNode('@test', new GenericTagValueNode(''))
+        );
+
+        // 元のメソッド名から `test` prefix を削除し, 1文字目を小文字に変更した文字列
+        $testName = \lcfirst(
+            (string) \preg_replace('/\Atest/', '', (string) $this->getName($node))
+        );
+        // `$testName` を新しいメソッド名として設定しなおす
+        $node->name = new Node\Identifier($testName);
+
         // change the node
         return $node;
+    }
+
+    private function shouldRefactor(ClassMethod $node): bool
+    {
+        // クラスメソッドが実装されているクラス情報を取得
+        $class = $this->betterNodeFinder->findParentType($node, ClassLike::class);
+
+        return
+            // メソッドの可視性が public かどうか
+            $node->isPublic()
+            // メソッド名が `test` から始まっているかどうか
+            && $this->isName($node, 'test*')
+            // メソッドが実装されているクラスが `PHPUnit\Framework\TestCase` を継承しているかどうか
+            && $this->nodeTypeResolver->isObjectType(
+                $class,
+                new ObjectType(TestCase::class),
+            );
     }
 }
